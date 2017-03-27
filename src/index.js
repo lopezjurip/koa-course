6877d4b1d26bb18110c568b7e861b9a3b43d7a1e
Patricio López <lopezjuripatricio@gmail.com>
2017-03-27 07:57:43 -0300

    Add middleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 'use strict';
 
 const Koa = require('koa');
+const logger = require('koa-logger');
+const axios = require('axios');
+
+const github = {
+  // This is a Promise!
+  user: id => axios.get(`https://api.github.com/users/${id}`),
+};
 
 const app = new Koa();
 
-app.use(ctx => {
-  ctx.body = {
-    message: 'Lorem ipsum',
-  };
+app.use(logger());
+
+app.use(async (ctx, next) => {
+  ctx.state.id = 'koajs';
+  await next();
+});
+
+app.use(async ctx => {
+  const { data } = await github.user(ctx.state.id);
+  ctx.body = data;
 });
 
 app.listen(3000);
